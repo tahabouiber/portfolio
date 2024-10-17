ffda0d4c039a583df1e1cdd4176a1a8d04e3604c
tahabouiber <tbouiber@gmail.com>
2024-10-17 11:26:38 +0200

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,14 +10,18 @@ import ScrollToTop from "/src/components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Taha Bouiber - Software Developer",
+  title: "Portfolio de Taha Bouiber",
   description:
     "Je m'appelle Taha Bouiber, et je suis un programmeur passionné et curieux, avec un fort esprit d'auto-apprentissage. J'aime explorer de nouvelles technologies et résoudre des problèmes complexes. Fortement intéressé par le développement web, logiciel et mobile, je m'efforce de créer des expériences utilisateur intuitives et élégantes. Compétent en JavaScript, j'utilise ce langage pour divers projets. Actuellement, je recherche un stage de fin d'études afin de mettre mes compétences en pratique et contribuer à des projets innovants."
-  };
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Add the favicon here */}
+        <link rel="icon" href="./favicon.ico" />
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
