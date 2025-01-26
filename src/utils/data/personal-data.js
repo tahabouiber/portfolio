b742efd7f2d9beab3cdd6ad7b325679e8d307989
tahabouiber <tbouiber@gmail.com>
2025-01-26 15:43:00 +0100

    email update

--- src/utils/data/personal-data.js
+++ src/utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: './profile.png',
   designation: "Développeur Logiciel",
   description : "Je m'appelle Taha Bouiber, et je suis un programmeur passionné et curieux, avec un fort esprit d'auto-apprentissage. J'aime explorer de nouvelles technologies et résoudre des problèmes complexes. Fortement intéressé par le développement web, logiciel et mobile, je m'efforce de créer des expériences utilisateur intuitives et élégantes. Compétent en JavaScript, j'utilise ce langage pour divers projets. Actuellement, je recherche un stage de fin d'études afin de mettre mes compétences en pratique et contribuer à des projets innovants.",
-  email: 'taha.bouiber@gmail.com',
+  email: 't.bouiber@gmail.com',
   phone: '+33 6 05 81 51 80',
   address: 'Le Mans, France ',
   github: 'https://github.com/tahabouiber',
