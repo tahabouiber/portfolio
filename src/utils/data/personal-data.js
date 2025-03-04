ac8c0238433a9f8ee7119a6b8547625871602406
tahabouiber <tbouiber@gmail.com>
2025-03-04 17:19:41 +0100

    cv update

--- src/utils/data/personal-data.js
+++ src/utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "TAHA BOUIBER",
   profile: './profile.png',
   designation: "Développeur Logiciel",
-  description : "Je m'appelle Taha Bouiber, et je suis un programmeur passionné et curieux, avec un fort esprit d'auto-apprentissage. J'aime explorer de nouvelles technologies et résoudre des problèmes complexes. Fortement intéressé par le développement web, logiciel et mobile, je m'efforce de créer des expériences utilisateur intuitives et élégantes. Compétent en JavaScript, j'utilise ce langage pour divers projets. Actuellement, je recherche un stage de fin d'études afin de mettre mes compétences en pratique et contribuer à des projets innovants.",
+  description : "Je m'appelle Taha Bouiber, et je suis un programmeur passionné et curieux, doté d'un fort esprit d'auto-apprentissage. J'aime explorer de nouvelles technologies et résoudre des problèmes complexes. Fortement intéressé par le développement web, logiciel et mobile, je m'efforce de créer des expériences utilisateur intuitives et élégantes. Compétent en JavaScript, Java et Python, j'utilise ces langages pour divers projets. Actuellement, je recherche un stage de fin d'études afin de mettre mes compétences en pratique et de contribuer à des projets innovants.",
   email: 't.bouiber@gmail.com',
   phone: '+33 6 05 81 51 80',
   address: 'Le Mans, France ',
@@ -10,5 +10,5 @@ export const personalData = {
   facebook: 'https://www.facebook.com/bouiber',
   linkedIn: 'https://www.linkedin.com/in/taha-bouiber/',
   twitter: 'https://x.com/TahaBouiber',
-  resume: "https://drive.google.com/file/d/1BgmoJsRN8riEVGwYysmASy5aPYwnwisE/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1kt2vVXIP5mlx2zE3bWi7YHUqtviHcIMu/view?usp=sharing"
 }
